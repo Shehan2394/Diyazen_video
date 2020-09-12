984d4f59e52b1ab737ffe96e185e08a3a4de782f
Shehan2394 <shehanweerasinghe5@gmail.com>
2020-09-12 12:56:52 +0530

    Diyazen_video

---
+++ android/app/src/main/kotlin/com/shehan/diyasenvideo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shehan.diyasenvideo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:video_player/video_player.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(VideoApp());
+
+class VideoApp extends StatefulWidget {
+  @override
+  _VideoAppState createState() => _VideoAppState();
+}
+
+class _VideoAppState extends State<VideoApp> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('video/Diyazen_Turntable.mp4')
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized, even before the play button has been pressed.
+        setState(() {
+          _controller.play();
+          _controller.setLooping(true);
+        });
+      });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Video Demo',
+      home: Scaffold(
+        body: Center(
+          child: _controller.value.initialized
+              ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child:GestureDetector(
+                child: VideoPlayer(_controller)),
+          )
+              : Container(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+}
+
